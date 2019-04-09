fda4baa71a7f769dfc24f701fba5608413801c6c
Amanda Rosen <amanda.hayley28@gmail.com>
2019-04-09 00:37:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function shout (string)
 {return string.toUpperCase()}
 function whisper (string)
 {return string.toLowerCase()}
+function shout (string)
+console.log(string.toUpperCase)
+
 function sayHiToGrandma (string)
 {return string.toLowerCase("I Can\'t hear you!")}
 console.log (sayHiToGrandma);
