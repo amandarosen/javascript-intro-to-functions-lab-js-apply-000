309e227377fc109d6018e40e7b87f5388cccb0b7
Amanda Rosen <amanda.hayley28@gmail.com>
2019-04-09 00:44:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function whisper (string)
 {return string.toLowerCase()}
 function logShout (string)
 {console.log(string.toUpperCase())}
-function whisper (string)
+function logWhisper (string)
 {console.log(string.toLowerCase())}
 function sayHiToGrandma (string)
 {if (string.toLowerCase () === string) return "I can\'t hear you!"}
